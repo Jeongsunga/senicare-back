ec0ba8e2ee181d1aa5d3ea6432ade1b35d5d9424
Jeongsunga <jsa3341@gmail.com>
2024-09-12 11:55:36 +0900

    24-09-12 11:50

--- src/main/java/com/korit/senicare/service/AuthService.java
+++ src/main/java/com/korit/senicare/service/AuthService.java
@@ -3,10 +3,12 @@ package com.korit.senicare.service;
 import org.springframework.http.ResponseEntity;
 
 import com.korit.senicare.dto.request.auth.IdCheckRequestDto;
+import com.korit.senicare.dto.request.auth.SignInRequestDto;
 import com.korit.senicare.dto.request.auth.SignUpRequestDto;
 import com.korit.senicare.dto.request.auth.TelAuthCheckRequestDto;
 import com.korit.senicare.dto.request.auth.TelAuthRequestDto;
 import com.korit.senicare.dto.response.ResponseDto;
+import com.korit.senicare.dto.response.auth.SignInResponseDto;
 
 public interface AuthService {
     
@@ -14,5 +16,7 @@ public interface AuthService {
     ResponseEntity<ResponseDto> telAuth(TelAuthRequestDto dto);
     ResponseEntity<ResponseDto> telAuthCheck(TelAuthCheckRequestDto dto);
     ResponseEntity<ResponseDto> signUp(SignUpRequestDto dto);
+    // 상위 ResponseDto도 사용하기 위함
+    ResponseEntity<? super SignInResponseDto> signIn(SignInRequestDto dto);   
 
 }
