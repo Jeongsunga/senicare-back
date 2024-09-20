21a826d6a5171dd4a0de2fce37dedcdcce01a882
Jeongsunga <jsa3341@gmail.com>
2024-09-20 10:02:13 +0900

    24-09-20 09:50

---
+++ src/main/java/com/korit/senicare/service/ToolService.java
@@ -0,0 +1,14 @@
+package com.korit.senicare.service;
+
+import org.springframework.http.ResponseEntity;
+
+import com.korit.senicare.dto.request.tool.PostToolRequestDto;
+import com.korit.senicare.dto.response.ResponseDto;
+import com.korit.senicare.dto.response.tool.GetToolListResponseDto;
+
+public interface ToolService {
+
+    ResponseEntity<ResponseDto> postTool(PostToolRequestDto dto);
+    ResponseEntity<? super GetToolListResponseDto> getToolList();
+    
+}
