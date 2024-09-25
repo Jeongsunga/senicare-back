b1419cfc865420afa6ac0d0ec4648656fa43a516
Jeongsunga <jsa3341@gmail.com>
2024-09-25 11:49:28 +0900

    24-09-25 11:50

--- src/main/java/com/korit/senicare/dto/response/ResponseMessage.java
+++ src/main/java/com/korit/senicare/dto/response/ResponseMessage.java
@@ -10,7 +10,8 @@ public interface ResponseMessage {
     String DUPLICATED_USER_ID = "Duplicated use id.";
     String DUPLICATED_TEL_NUMBER = "Duplicated user tel number.";
     String NO_EXIST_USER_ID = "No exist user id.";
-    String NO_EXIST_TOOL = "No exist tool";
+    String NO_EXIST_TOOL = "No exist tool.";
+    String NO_EXIST_CUSTOMER = "No exist customer.";
 
     String TEL_AUTH_FAIL = "Tel number authentication failed.";
     String SIGN_IN_FAIL = "Sign in failed.";
